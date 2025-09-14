089b24674829f4285c18c235112e5c322e011441
vini <vinithavijayakumar2403@gmail.com>
2025-09-14 06:46:14 +0000

    adding new file

---
+++ var.tf
@@ -0,0 +1,17 @@
+variable "project" {
+  description = "The GCP project ID"
+  type        = string
+  default     = "banded-oven-469716-n3"
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-east1"
+}
+
+variable "credentials_file" {
+  description = "Path to the GCP service account key JSON file"
+  type        = string
+}
+
